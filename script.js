5ee8e6f0d6934e739b5e3e9e885b6800f99ec5bd
LordRingono <40274235+LordRingono@users.noreply.github.com>
2024-06-17 17:57:32 +0000

    added input and bot response

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 var myGame = new WizardOrpheus('', `
 You are a haunted house that is alive. Try to take out the person walking through you as quickly as possible.
 `)
+
+myGame.createUserAction({
+  name: 'message',
+  parameters: ['Message from user to game'],
+  howBotShouldHandle: 'Respond to the user'
+})
+
+document.getElementById('input').addEventListener('keyup', function(e) {
+  if (e.code == 'Enter') { // if the user presses enter
+    let userInput = document.getElementById('input').value
+    myGame.message(userInput)
+
+    document.getElementById('conversation').innerHTML += '<p>' + userInput + '</p>'
+
+    document.getElementById('input').value = ''
+  }
+})
+myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
+  // Add the bot's response to the conversation
+  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+})
